a5d5349ece2a73a79f93cb50ec6ea162c670a386
Breno Souza <156030544+brenojose@users.noreply.github.com>
2024-02-23 03:23:58 -0500

    Update J2Controller.cs

--- Assignment 2/Controllers/J2Controller.cs
+++ Assignment 2/Controllers/J2Controller.cs
@@ -1,11 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Net.NetworkInformation;
 using System.Web.Http;
-using System.Web.Http.Results;
 
 namespace Assignment02.Controllers
 {
