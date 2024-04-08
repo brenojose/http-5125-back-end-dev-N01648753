93ddb3e94adf386957cd42e6a00afff04455bb75
Breno Souza <156030544+brenojose@users.noreply.github.com>
2024-04-08 14:44:15 -0400

    Add files via upload

---
+++ C2_SchoolProject/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace C1_SchoolProject.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
